29cc971135476977728fbaddc96a564cb40d0530
wolftic <lorenzokramertje@gmail.com>
2016-10-12 16:07:59 +0200

    Init

---
+++ Assets/Scripts/SpriteLayerSorterer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteLayerSorterer : MonoBehaviour {
+	private SpriteRenderer sprite;
+
+	void Start () {
+		sprite = GetComponent<SpriteRenderer> ();
+	}
+
+	void Update () {
+		sprite.sortingOrder = -Mathf.RoundToInt(transform.position.y * 2f);
+	}
+}
